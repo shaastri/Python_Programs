fda96385610760d712a3106353d9e7705ff915b0
Jangam Bheemeswara Sastry <85690072+shaastri@users.noreply.github.com>
2023-03-16 15:46:41 +0530

    Practice Programs

---
+++ add.py
@@ -0,0 +1,6 @@
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+
+print("The addition of two numbers is:", add(10, 15))
---
+++ sub.py
@@ -0,0 +1,6 @@
+def sub(num1, num2):
+    difference = num1 - num2
+    return difference
+
+
+print("The substraction of two numbers is:", sub(10, 15))
